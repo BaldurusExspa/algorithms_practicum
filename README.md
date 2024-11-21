eb6e133f3a8c033c12167b46a44f9b70c6c9cc91
BaldurusExspa <maksimzelenskij619@gmail.com>
2024-11-21 16:46:43 +0300

    Add inquirer library

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Fibonacci Algorithms
 ### This repository for algorithms that calculate the Fibonacci number
 # Launch
-### To get started you will need to install the following install:
+### To get started you will need to the following install:
 
 - Node: ```~> v20.14.0```
+- Inquirer: ```~> 12.1.0 ```
 
-### To start the project, run the following command:
-Recursive method:
+### To install dependencies, run the following command:
+```sh
+npm install
 ```
-node .\index.js
+### To start the project, run the following command:
+```sh
+npm run start
 ```
--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-import fib from "./modules/fib_loop.js";
-// import { fib as fib_recursive} from "./modules/fib_recursive.js";
+import inquirer from "inquirer";
 import { benchmark } from "./benchmark.js";
 
 const testNum = 12;
-const resultBench = benchmark(fib(testNum));
-const resultFunc = fib(testNum);
+// const resultBench = benchmark(fib(testNum));
+// const resultFunc = fib(testNum);
 
-console.log(`Получилось число: ${resultFunc}; Время нахождения этого числа = ${resultBench};`);
+const answers_list = [
+    {
+        name: 'Naive recursive algorithm',
+        value: 'fib_recursive',
+        module: './modules/fib_recursive.js'
+    },
+    {
+        name: 'Classic iteractive  algorithm',
+        value: 'fib_loop',
+        module: './modules/fib_loop.js'
+    }
+]
+
+inquirer
+  .prompt([
+    {
+      type: "rawlist",
+      name: "algorithm",
+      message: "What algorithm you need?",
+      choices: [
+        ...answers_list
+      ]
+    },
+    {
+      type: 'number',
+      name: 'Number',
+      message: 'Your number:'
+    }
+  ])
+  .then((answers) => {
+    console.info("Answer:", answers);
+  });
